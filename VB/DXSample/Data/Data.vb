1d5d821d6aeec88eeb488ce530905aa60889bdf6
DevExpressExampleBot <fakeEmail>
2020-04-09 14:15:33 +0300

    Source auto update [skip ci]

---
+++ VB/DXSample/Data/Data.vb
@@ -0,0 +1,20 @@
+﻿Imports System
+
+Namespace DXSample.Data
+	Public Class AppointmentEntity
+		Public Property Id() As Integer
+		Public Property Subject() As String
+		Public Property Description() As String
+		Public Property Start() As DateTime
+		Public Property [End]() As DateTime
+		Public Property AppointmentType() As Integer
+		Public Property RecurrenceInfo() As String
+		Public Property ResourceId() As Integer
+		Public Property Label() As Integer
+	End Class
+
+	Public Class ResourceEntity
+		Public Property Id() As Integer
+		Public Property Description() As String
+	End Class
+End Namespace
---
+++ VB/DXSample/MainWindow.xaml.vb
@@ -0,0 +1,12 @@
+﻿Imports DevExpress.Xpf.Core
+
+Namespace DXSample
+	Partial Public Class MainWindow
+		Inherits ThemedWindow
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+	End Class
+End Namespace
