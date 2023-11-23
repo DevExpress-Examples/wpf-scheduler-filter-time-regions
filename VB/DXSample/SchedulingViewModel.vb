80b557571cc50d1ee78829294777406fc1ef4ffc
DevExpressExampleBot <fakeEmail>
2023-11-23 17:39:46 +0400

    Source auto update [skip ci]

--- VB/DXSample/SchedulingViewModel.vb
+++ VB/DXSample/SchedulingViewModel.vb
@@ -1,46 +1,47 @@
-﻿Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm
 Imports DevExpress.Mvvm.Native
 Imports DXSample.Data
 Imports System.Collections.ObjectModel
 
 Namespace DXSample
-	Public Class SchedulingViewModel
-		Inherits ViewModelBase
 
-		Protected _Appts As New ObservableCollection(Of AppointmentEntity)()
-		Public ReadOnly Property Appts() As ObservableCollection(Of AppointmentEntity)
-			Get
-				Return Me._Appts
-			End Get
-		End Property
+    Public Class SchedulingViewModel
+        Inherits ViewModelBase
 
+        Protected _Appts As ObservableCollection(Of AppointmentEntity) = New ObservableCollection(Of AppointmentEntity)()
 
-		Protected _Calendars As New ObservableCollection(Of ResourceEntity)()
-		Public ReadOnly Property Calendars() As ObservableCollection(Of ResourceEntity)
-			Get
-				Return Me._Calendars
-			End Get
-		End Property
+        Public ReadOnly Property Appts As ObservableCollection(Of AppointmentEntity)
+            Get
+                Return _Appts
+            End Get
+        End Property
 
-		Public Sub New()
-			Calendars.Add(DataHelper.Personal())
-			Calendars.Add(DataHelper.Education())
-			Calendars.Add(DataHelper.Work())
+        Protected _Calendars As ObservableCollection(Of ResourceEntity) = New ObservableCollection(Of ResourceEntity)()
 
-			DataHelper.Gym().ForEach(Sub(c) Appts.Add(c))
-			Appts.Add(DataHelper.Dentist())
-			Appts.Add(DataHelper.Dinner())
-			Appts.Add(DataHelper.Disneyland())
-			Appts.Add(DataHelper.RR())
-			Appts.Add(DataHelper.DayOff())
-			Appts.Add(DataHelper.SecondShift())
-			Appts.Add(DataHelper.ConferenceCompanyMeeting())
-			Appts.Add(DataHelper.ConferenceCustomerRetentionReview())
-			Appts.Add(DataHelper.ConferenceDatabaseAndWebsiteReview())
-			Appts.Add(DataHelper.ConferenceWeeklyMeeting())
-			Appts.Add(DataHelper.TrainingFrenchLesson())
-			Appts.Add(DataHelper.TrainingGermanLesson())
-			Appts.Add(DataHelper.TrainingTrainStaffOnNewRemoteControls())
-		End Sub
-	End Class
+        Public ReadOnly Property Calendars As ObservableCollection(Of ResourceEntity)
+            Get
+                Return _Calendars
+            End Get
+        End Property
+
+        Public Sub New()
+            Calendars.Add(DataHelper.Personal())
+            Calendars.Add(DataHelper.Education())
+            Calendars.Add(DataHelper.Work())
+            Call DataHelper.Gym().ForEach(Sub(c) Appts.Add(c))
+            Appts.Add(DataHelper.Dentist())
+            Appts.Add(DataHelper.Dinner())
+            Appts.Add(DataHelper.Disneyland())
+            Appts.Add(DataHelper.RR())
+            Appts.Add(DataHelper.DayOff())
+            Appts.Add(DataHelper.SecondShift())
+            Appts.Add(DataHelper.ConferenceCompanyMeeting())
+            Appts.Add(DataHelper.ConferenceCustomerRetentionReview())
+            Appts.Add(DataHelper.ConferenceDatabaseAndWebsiteReview())
+            Appts.Add(DataHelper.ConferenceWeeklyMeeting())
+            Appts.Add(DataHelper.TrainingFrenchLesson())
+            Appts.Add(DataHelper.TrainingGermanLesson())
+            Appts.Add(DataHelper.TrainingTrainStaffOnNewRemoteControls())
+        End Sub
+    End Class
 End Namespace
